58970f5a6a52562522cbabfb769d2c396d2485ca
oglucasvhb <lucasdhenrique58@gmail.com>
2026-02-12 21:39:54 -0300

    Aula 02: Tipos de dados e variáveis do sistema

---
+++ teste.py
@@ -0,0 +1,8 @@
+nome = "lucas"
+idade = 16
+altura = 1.7
+estudante = "luiz henrique"
+
+relatorio = f"seu  nome é {nome} eu voce tem {idade} anos sua altura é {altura} e estuda na escola {estudante} "
+
+print(f"{relatorio}")
