74d0787e61f2e61a2f89cd4ad7926fa29190fe3c
oglucasvhb <lucasdhenrique58@gmail.com>
2026-02-19 21:59:15 -0300

    quarta aula

---
+++ modelos.py
@@ -0,0 +1,12 @@
+"""
+
+1- nome  = o cliente fornece o nome de sua empresa
+2-  local= o cliente fornece o local para entrega
+3 - telefone  = cliente fornece seu telefone 
+4 - cnpj =  a empresa fornece o cnpj para retirarmos a nota do seu pedido no cnpj do comércio
+5 - responsavel = o cliente informa alguém responsável pelo 
+
+
+"""
+
+class cliente:
